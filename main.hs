9e1b62ad9c5ffe4d62b2642d2592754cdf71282b
Milan Tichavský <milantichavsky@seznam.cz>
2024-02-26 21:16:18 +0100

    Bring first part home (only edgecases are missing)
    and maybe some refactor

--- main.hs
+++ main.hs
@@ -6,30 +6,66 @@ import Text.Read
 dispatch :: [(String, [String] -> IO ())]
 dispatch = [("-1", classify), ("-2", train)]
 
+main :: IO ()
 main = do
     (command : args) <- getArgs
     let (Just action) = lookup command dispatch
     action args
 
-data Tree = EmptyTree | Leaf String | Node (Integer, Float) Tree Tree deriving (Show)
+data Tree = EmptyTree | Leaf String | Node (Int, Float) Tree Tree deriving (Show)
 
 classify :: [FilePath] -> IO ()
 classify (tree_filename:args) = do
     handle <- openFile tree_filename ReadMode
     tree <- buildTree handle 0
-    print tree
     hClose handle
-    
-    -- writeFile dest contents
+    c <- classify' tree args
+    print c
+
+classify' :: Tree -> [FilePath] -> IO ()
+classify' tree [filepath] = 
+    withFile filepath ReadMode (\handle -> do
+        classifyLines handle tree
+    )
+classify' _ (_:_) = 
+    error "Too much arguments provided"
+classify' _ _ = 
+    error "Argument is missing"
+
+classifyLines :: Handle -> Tree -> IO ()
+classifyLines handle tree = do
+    line <- hGetLine handle
+    classifyOneLine (words [if c == ',' then ' ' else c|c <- line]) tree
+    eof <- hIsEOF handle
+    if eof then
+        return ()
+    else
+        classifyLines handle tree
+
+classifyOneLine:: [String] -> Tree -> IO ()
+classifyOneLine values tree = do
+    case tree of
+        (Node (i, t) l r) -> do
+            let value = values !! i
+            if toNumber value <= t then
+                classifyOneLine values l
+            else
+                classifyOneLine values r
+        (Leaf c) -> do
+            print c
+        _ -> error "Unknown element in the tree!"
+
 
 train :: [FilePath] -> IO ()
 train (trainingData:_) = return ()
 
+toNumber :: Read a => String -> a
 toNumber x =
     case readMaybe x of
         Just number -> number
         Nothing -> error "Poorly formatted file, failed to parse a number"
 
+buildTree :: Handle -> Int -> IO Tree
 buildTree handle level = do
     -- TODO: I could check for whitespace characters in here
     line <- hGetLine handle
@@ -43,3 +79,5 @@ buildTree handle level = do
         ["Leaf:", c] -> 
             return $ Leaf c
         _ -> error "Poorly formatted input file"
+
+-- TODO what if those files have newlines at the end?
